6df51089045c1607ed99b7bd51e8b63c8a0a8b13
Richu Antony <96441125+Richu-Antony@users.noreply.github.com>
2023-09-22 11:57:02 +0530

    Change

---
+++ P1 Quiz.py
@@ -0,0 +1,88 @@
+print("\nWelcome To Quiz Program")
+
+playig = input("\nDo you want to play? ")
+
+if playig != "yes":
+    quit()
+
+print("\nOkay Lets go :) ")
+
+
+# Question 1
+answer = input("\nWhat is CPU stands for? ")
+if answer == "Central Processing Unit" or answer == "Central processing unit":
+    print("Correct Answer")
+else:
+    print("Incorrect Answer")
+
+
+# Question 2
+answer = input("\nWhat is RAM stands for? ")
+if answer == "Random Access Memory" or answer == "Random access memory":
+    print("Correct Answer")
+else:
+    print("Incorrect Answer")
+
+
+# Question 3
+answer = input("\nWhat is ROM stands for? ")
+if answer == "Read Only Memory" or answer == "Read only memory":
+    print("Correct Answer")
+else:
+    print("Incorrect Answer")
+
+
+# Question 4
+answer = input("\nWhat is GPU stands for? ")
+if answer == "Graphics Processing Unit":
+    print("Correct Answer")
+else:
+    print("Incorrect Answer")
+
+
+# Question 5
+answer = input("\nWhat is USB stands for? ")
+if answer == "Unified System Bus":
+    print("Correct Answer")
+else:
+    print("Incorrect Answer")
+
+
+# Question 6
+answer = input("\nWhat is PSU stands for? ")
+if answer == "Power Supply Unit":
+    print("Correct Answer")
+else:
+    print("Incorrect Answer")
+
+
+# Question 7
+answer = input("\nWhat is CPU stands for? ")
+if answer == "Central Processing Unit":
+    print("Correct Answer")
+else:
+    print("Incorrect Answer")
+
+
+# Question 8
+answer = input("\nWhat is CPU stands for? ")
+if answer == "Central Processing Unit":
+    print("Correct Answer")
+else:
+    print("Incorrect Answer")
+
+
+# Question 9
+answer = input("\nWhat is CPU stands for? ")
+if answer == "Central Processing Unit":
+    print("Correct Answer")
+else:
+    print("Incorrect Answer")
+
+
+# Question 10
+answer = input("\nWhat is CPU stands for? ")
+if answer == "Central Processing Unit":
+    print("Correct Answer")
+else:
+    print("Incorrect Answer")
